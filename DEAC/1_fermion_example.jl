db0556e9a284c2bf4e2f40810afe468dd06eb0ed
James Neuhaus <james.neuhaus@gmail.com>
2026-05-28 11:36:16 -0400

    your ad goes here

--- DEAC/1_fermion_example.jl
+++ DEAC/1_fermion_example.jl
@@ -28,47 +28,47 @@ function run_example()
     n_runs_per_bin = 100
 
 
-    # # DEAC on G(0,0) for DOS
-    # # using covariance matrix
-    # deac_dict = DEAC_Binned(
-    #     G_r, # shape (bins,taus)
-    #     beta,
-    #     taus,
-    #     ws,
-    #     kernel_mode, # "time_fermionic" or "time_bosonic"
-    #     n_bins, # number of bins to split the data into
-    #     n_runs_per_bin, # number of runs to perform per bin
-    #     "results/DEAC_DOS_cov.jld2", # output file
-    #     "chk.jld2", # checkpoint file
-    #     find_fitness_floor = false,
-    #     number_of_generations = 20_000, #max number of generations within a run
-    #     keep_bin_data = false, # keep the bin data and store it in the output file
-    #     verbose = true, # print progress
-    #     fitness = [1000.0, 100.0, 10.0, 1.0]#, 0.5], # target χ² fitnesses for the DEAC algorithm
-    #                                                     # Typically, you would only want to use 1.0 unless that overfits.
-    #                                                     # I am doing multiple fitnesses to show you how DEAC converges
-    # )
+    # DEAC on G(0,0) for DOS
+    # using covariance matrix
+    deac_dict = DEAC_Binned(
+        G_r, # shape (bins,taus)
+        beta,
+        taus,
+        ws,
+        kernel_mode, # "time_fermionic" or "time_bosonic"
+        n_bins, # number of bins to split the data into
+        n_runs_per_bin, # number of runs to perform per bin
+        "results/DEAC_DOS_cov.jld2", # output file
+        "chk.jld2", # checkpoint file
+        find_fitness_floor = false,
+        number_of_generations = 20_000, #max number of generations within a run
+        keep_bin_data = false, # keep the bin data and store it in the output file
+        verbose = true, # print progress
+        fitness = [1000.0, 100.0, 10.0, 1.0]#, 0.5], # target χ² fitnesses for the DEAC algorithm
+                                                        # Typically, you would only want to use 1.0 unless that overfits.
+                                                        # I am doing multiple fitnesses to show you how DEAC converges
+    )
 
-    # # using standard deviation
-    # deac_dict = DEAC_Std(
-    #     mean(G_r,dims=1)[1,:], # shape (taus)
-    #     std(G_r,dims=1)[1,:], # shape (taus)
-    #     beta,
-    #     taus,
-    #     ws,
-    #     kernel_mode, # "time_fermionic" or "time_bosonic"
-    #     n_bins, # number of bins to split the data into
-    #     n_runs_per_bin, # number of runs to perform per bin
-    #     "results/DEAC_DOS_std.jld2", # output file
-    #     "chk.jld2", # checkpoint file
-    #     find_fitness_floor = false,
-    #     number_of_generations = 20_000, #max number of generations within a run
-    #     keep_bin_data = false, # keep the bin data and store it in the output file
-    #     verbose = true, # print progress
-    #     fitness =  [ 100.0, 10.0, 1.0, 0.1, 0.01, 0.001,  0.0005]#0.0001], # target χ² fitnesses for the DEAC algorithm
-    #                                                     # Typically, you would only want to use 1.0 unless that overfits.
-    #                                                     # I am doing multiple fitnesses to show you how DEAC converges
-    # )
+    # using standard deviation
+    deac_dict = DEAC_Std(
+        mean(G_r,dims=1)[1,:], # shape (taus)
+        std(G_r,dims=1)[1,:], # shape (taus)
+        beta,
+        taus,
+        ws,
+        kernel_mode, # "time_fermionic" or "time_bosonic"
+        n_bins, # number of bins to split the data into
+        n_runs_per_bin, # number of runs to perform per bin
+        "results/DEAC_DOS_std.jld2", # output file
+        "chk.jld2", # checkpoint file
+        find_fitness_floor = false,
+        number_of_generations = 20_000, #max number of generations within a run
+        keep_bin_data = false, # keep the bin data and store it in the output file
+        verbose = true, # print progress
+        fitness =  [ 100.0, 10.0, 1.0, 0.1, 0.01, 0.001,  0.0005]#0.0001], # target χ² fitnesses for the DEAC algorithm
+                                                        # Typically, you would only want to use 1.0 unless that overfits.
+                                                        # I am doing multiple fitnesses to show you how DEAC converges
+    )
 
     # DEAC on G(k,0) for spectral function
     for k in 15:16
--- DEAC/2_boson_example.jl
+++ DEAC/2_boson_example.jl
@@ -23,30 +23,30 @@ function run_example()
     ws = collect(LinRange(0.0, 20.0, 1001))
     beta = 20.0
     kernel_mode = "time_bosonic_symmetric_w"
-    n_bins = 10
+    n_bins = 100
     n_runs_per_bin = 100
 
 
     # DEAC on G(0,0) for DOS
     # using covariance matrix
-    # deac_dict = DEAC_Binned(
-    #     curr[:,1:101], # shape (bins,taus)
-    #     beta,
-    #     taus[1:101],
-    #     ws,
-    #     kernel_mode, # "time_fermionic" or "time_bosonic"
-    #     n_bins, # number of bins to split the data into
-    #     n_runs_per_bin, # number of runs to perform per bin
-    #     "results/DEAC_curr_cov.jld2", # output file
-    #     "chk.jld2", # checkpoint file
-    #     find_fitness_floor = false,
-    #     number_of_generations = 20_000, #max number of generations within a run
-    #     keep_bin_data = false, # keep the bin data and store it in the output file
-    #     verbose = true, # print progress
-    #     fitness = [1000.0, 100.0, 10.0, 2.0, 1.3, 1.2,1.0], # target χ² fitnesses for the DEAC algorithm
-    #                                                     # Typically, you would only want to use 1.0 unless that overfits.
-    #                                                     # I am doing multiple fitnesses to show you how DEAC converges
-    # )
+    deac_dict = DEAC_Binned(
+        curr[:,1:101], # shape (bins,taus)
+        beta,
+        taus[1:101],
+        ws,
+        kernel_mode, # "time_fermionic" or "time_bosonic"
+        n_bins, # number of bins to split the data into
+        n_runs_per_bin, # number of runs to perform per bin
+        "results/DEAC_curr_cov.jld2", # output file
+        "chk.jld2", # checkpoint file
+        find_fitness_floor = false,
+        number_of_generations = 20_000, #max number of generations within a run
+        keep_bin_data = false, # keep the bin data and store it in the output file
+        verbose = true, # print progress
+        fitness = [1000.0, 100.0, 10.0, 2.0, 1.3, 1.2,1.0], # target χ² fitnesses for the DEAC algorithm
+                                                        # Typically, you would only want to use 1.0 unless that overfits.
+                                                        # I am doing multiple fitnesses to show you how DEAC converges
+    )
 
     # using standard deviation
     deac_dict = DEAC_Std(
